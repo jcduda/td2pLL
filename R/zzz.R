b542b9e152ff6dcf153270918d1842291bd6c36d
jcduda <julia.duda@tu-dortmund.de>
2021-03-01 16:52:19 +0100

    Add imports. Note that roxygen does not put them into NAMESPACE. Only manually re-write NAMESPACE

---
+++ R/zzz.R
@@ -0,0 +1,5 @@
+#' @importFrom dplyr mutate filter select group_by summarize '%>%'
+#' @importFrom stats anova coef nls predict
+#' @importFrom drc LL2.2 LL.2 drmc
+#' @importFrom tidyr pivot_wider
+#' @importFrom rlang .data
