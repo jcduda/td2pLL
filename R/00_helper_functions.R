5136ecc597f7197e2fe24377e0a698105384a66a
jcduda <julia.duda@tu-dortmund.de>
2021-03-01 10:55:53 +0100

    Added get_EC50s helper function in new script made for helper functions

---
+++ R/00_helper_functions.R
@@ -0,0 +1,27 @@
+
+
+
+#' @title Accessing EC50 values of a td2pLL model
+#'
+#' @description For parameters of a td2pLL model
+#' and given \code{time} values, the EC50 value is retrieved.
+#' @param coefs Named numeric vector containing the parameters of a td2pLL
+#' model containing the coefficients h, delta, gamma and c0.
+#' For an object of class td2pLL, these can be accessed via \code{coef()}.
+#' @param times Numeric vector of the times for which the EC50 shall be retrieved.
+#' Entries must be greater than 0.
+#' @return A data.frame with columns time and EC50 containing the EC50s at
+#'  \code{times}.
+
+
+get_EC50s <- function(coefs, times) {
+  stopifnot(length(setdiff(names(coefs), c("h", "delta", "gamma", "c0"))) == 0)
+  stopifnot(is.numeric(times) & length(times) >= 1)
+  stopifnot(all(times >= 0))
+
+  EC50s <- sapply(expos, function(expo) coefs["delta"] * expo^(-coefs["gamma"]) + coefs["c0"])
+
+  res <- data.frame(time = times, EC50 = EC50s)
+
+  return(res)
+}
