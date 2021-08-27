1bf506c73dee9e4f8282b92fa80ed14e8b8231ac
jcduda <julia.duda@tu-dortmund.de>
2021-08-27 13:41:52 +0200

    Fixed: interpolating ED50 did not work if before-response was zero

--- R/interp_ED50.R
+++ R/interp_ED50.R
@@ -22,12 +22,20 @@ interp_ED50 <- function(data = NULL) {
     dose_bef_after <- data$dose[id_bef_after]
     resp_bef_after <- data$mean_resp[id_bef_after]
 
-    # intercept
-    b <- (resp_bef_after[2] - resp_bef_after[1] *
-            dose_bef_after[2] / dose_bef_after[1]) /
-      (1 - dose_bef_after[2] / dose_bef_after[1])
+    # # intercept
+    # b <- (resp_bef_after[2] - resp_bef_after[1] *
+    #         dose_bef_after[2] / dose_bef_after[1]) /
+    #   (1 - dose_bef_after[2] / dose_bef_after[1])
+    # # slope
+    # m <- (resp_bef_after[2] - b) / dose_bef_after[2]
+
     # slope
-    m <- (resp_bef_after[2] - b) / dose_bef_after[2]
+    m <- (resp_bef_after[2] - resp_bef_after[1]) /
+      (dose_bef_after[2] - dose_bef_after[1])
+
+    # intercept
+    b <- resp_bef_after[1] - m * dose_bef_after[1]
+
 
     ED50_interp <- (50 - b) / m
   }
