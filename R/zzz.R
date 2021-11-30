69fad8842602b9430b2724e7f06f4250da8b0018
jcduda <julia.duda@tu-dortmund.de>
2021-11-30 15:53:50 +0100

    Fix package check error on imported but not required (in description) functions

--- R/zzz.R
+++ R/zzz.R
@@ -3,10 +3,16 @@
 #' @importFrom drc LL2.2 LL.2 drmc
 #' @importFrom minpack.lm nlsLM
 #' @importFrom tidyr pivot_wider
+#' @import ggplot2
+#' @importFrom ggrepel geom_text_repel
 #' @importFrom rlang .data
 #' @importFrom graphics persp
+#' @importFrom grDevices dev.off jpeg
+#' @importFrom stats heatmap time
 #' @importFrom utils setTxtProgressBar txtProgressBar
 #' @importFrom MASS ginv
-#' @import magick
+#' @importFrom magick image_read image_join image_animate image_write
 #' @import ICAOD
 NULL
+
+
