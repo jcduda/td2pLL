feff7f9174fca4c374f17b3eff67dc1f3f51224a
jcduda <julia.duda@tu-dortmund.de>
2021-11-30 15:56:28 +0100

    Removed spaces

--- R/icaodOptDesign.R
+++ R/icaodOptDesign.R
@@ -19,32 +19,44 @@
 #'  typical td2pLL model is, for optima design planning, always assumed to be
 #'  unknown as otherwise no control measurements in dose=0 would be porposed
 #'  by the optimal design.
-#'  @param lx (`numeric(2)`) \cr
+#' @param lx (`numeric(2)`) \cr
 #'   Minimal value for time and dose. Note that for dose, you cannot give
 #'   a lower bound of zero due to technical reasons.
 #'   Instead, provide a very small lower bound (default is exp(-8) = 0.00034)
 #'   that represents zero.
-#'  @param ux (`numeric(2)`) \cr
+#' @param ux (`numeric(2)`) \cr
 #'   Maximal value for time and dose.
-#'  @param iter (`integer(1)`) \cr
+#' @param iter (`integer(1)`) \cr
 #'   Number of iterations in the Imperialist Competitive Algorithm. Default is 1000.
-#'  @param ICA.control(`list()`) \cr
-#'   Additional control parameters passed to the [ICAOD::locally] function.
-#'   See [ICAOD::ICA.control()] for details.
-#'  @return An object of class `minimax` generated with [ICAOD::locally()].
+#' @param ICA.control (`list()`) \cr
+#'   Additional control parameters passed to the [locally()] function.
+#'   See [ICA.control()] for details.
+#' @return An object of class `minimax` generated with [locally()].
 #'   For `Emax_known` = `TRUE`, the design has 7 support points. Otherwise,
 #'   it has 8.
-#'  @examples
+#' @examples
 #'  td_opt_1 <- td_opt(param = c(h = 2, delta = 0.2, gamma = 1.3, c0 = 0.2),
-#'    Emax_known = T, ICA.control = c(ncount = 300, rseed = 1905))
+#'    Emax_known = TRUE,
+#'     ICA.control = list(ncount = 300, rseed = 1905, trace = FALSE),
+#'     iter = 600)
+#'  td_opt_1
 #'  plot_td_des(td_opt_1)
-#'  plot_td_dcrit_equ(td_opt_2)
+#'  plot_td_dcrit_equ(td_opt_1)
 #'  td_opt_2 <- td_opt(param = c(h = 2, delta = 0.2, gamma = 1.3, c0 = 0.2),
-#'    Emax_known = F, ICA.control = c(ncount = 300, rseed = 1905))
+#'    Emax_known = FALSE,
+#'     ICA.control = list(ncount = 300, rseed = 1905, revol_rate = 0.5, trace = FALSE),
+#'     iter = 600)
+#'  td_opt_2
+#'  plot_td_des(td_opt_2)
+#'  plot_td_dcrit_equ(td_opt_2)
+#'  td_opt_3 <- td_opt(param = c(h = 1, delta = 0.5, gamma = 2, c0 = 0.01),
+#'    Emax_known = TRUE, ICA.control = list(ncount = 300, rseed = 1905, trace = FALSE),
+#'     iter = 1000)
+#'  plot_td_des(td_opt_3)
+#'  plot_td_dcrit_equ(td_opt_3, plot_theta = 100, n_grid = 100, dose_lim = c(0, 0.2))
 
 
-
-td_opt <- function(param, Emax_known = F, lx = c(1, exp(-8)), ux = c(10, 1),
+td_opt <- function(param, Emax_known = FALSE, lx = c(1, exp(-8)), ux = c(10, 1),
                    iter = 1000, ICA.control = NULL){
 
   stopifnot(all(names(param) == c("h", "delta", "gamma", "c0")))
@@ -62,7 +74,7 @@ td_opt <- function(param, Emax_known = F, lx = c(1, exp(-8)), ux = c(10, 1),
 
   if(is.null(ICA.control)) ICA.control <- list(ncount = 100)
 
-  icaod_opt <- ICAOD::locally(predvars = c("time", "dose"),
+  icaod_opt <- locally(predvars = c("time", "dose"),
                               parvars = parvars,
                               fimfunc= fimfunc,
                               npar = ifelse(Emax_known, 5, 6),
